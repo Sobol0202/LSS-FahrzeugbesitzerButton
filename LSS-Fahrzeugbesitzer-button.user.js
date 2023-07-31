51d2ddb07201980c2f48163e5b51d5cbe861011a
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2023-07-31 14:53:38 +0200

    Create LSS-Fahrzeugbesitzer-button.user.js

---
+++ LSS-Fahrzeugbesitzer-button.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Fahrzeugbesitzer-Button
+// @namespace    www.leitstellenspiel.de
+// @version      1.0
+// @description  Fügt einen Button neben dem Fahrzeugbesitzer-Namen hinzu, der den Namen in den Input mit der ID "mission_reply_content" einfügt.
+// @author       MissSobol
+// @match        https://www.leitstellenspiel.de/missions/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Funktion zum Einfügen des Buttons und Hinzufügen des Klick-Handlers
+    function addButtonToOwner() {
+        const rows = document.querySelectorAll('tr[id^="vehicle_row_"]');
+
+        rows.forEach(row => {
+            const ownerLink = row.querySelector('td:nth-child(5) a[href^="/profile/"]');
+            if (ownerLink && !ownerLink.parentElement.querySelector('button[data-owner-button]')) {
+                const ownerName = ownerLink.textContent.trim();
+                const button = document.createElement('button');
+                button.textContent = '@';
+                button.style.marginLeft = '5px';
+                button.dataset.ownerButton = true;
+                button.onclick = function() {
+                    const input = document.getElementById('mission_reply_content');
+                    input.value += '@' + ownerName + ' ';
+                };
+
+                ownerLink.parentElement.appendChild(button);
+            }
+        });
+    }
+
+    addButtonToOwner();
+})();
