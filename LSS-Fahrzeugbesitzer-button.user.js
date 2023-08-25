f005b6986e5eb79bafc98f9a8f8e2985f2c5e0ac
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2023-08-25 18:21:13 +0200

    Update LSS-Fahrzeugbesitzer-button.user.js

--- LSS-Fahrzeugbesitzer-button.user.js
+++ LSS-Fahrzeugbesitzer-button.user.js
@@ -1,15 +1,69 @@
 // ==UserScript==
 // @name         Fahrzeugbesitzer-Button
 // @namespace    www.leitstellenspiel.de
-// @version      1.3
+// @version      1.4r
 // @description  Fügt einen Button neben dem Fahrzeugbesitzer-Namen hinzu, der den Namen in das RMS einfügt.
 // @author       MissSobol
 // @match        https://www.leitstellenspiel.de/missions/*
 // @grant        none
 // ==/UserScript==
 
-//Lieber Scriptnutzer,
-//schweren Herzens habe ich heute alle meine Scripte deaktiviert. Der Betreiber hat mir meinen Forenaccount gesperrt, will mir aber weder verraten warum, noch ist man gewillt in Diskurs mit mir zu treten. Da ich ohne einen Forenaccount nicht das Level an Support geben kann, welches ich für mich selber als erforderlich betrachte, habe ich mit sofortiger Wirkung alle meine Scripte deaktiviert.
-//Ich bedanke mich für 5 Jahre Zusammenarbeit im Forum. Danke an alle, die meine Scripte genutzt haben, Daumen dagelassen haben und Kommentare geschrieben haben.
-//Liebe Grüße und noch viel Spaß
-//Sobol
+(function() {
+    'use strict';
+
+    // Funktion zum Überprüfen, ob der Darkmode aktiv ist
+    function isDarkMode() {
+        const bodyBackgroundColor = window.getComputedStyle(document.body).backgroundColor;
+        return bodyBackgroundColor !== 'rgb(250, 250, 250)'; // #fafafa
+    }
+
+    // Funktion zum Einfügen des Buttons und Hinzufügen des Klick-Handlers
+    function addButtonToOwner(ownerLink) {
+        if (ownerLink && !ownerLink.parentElement.querySelector('button[data-owner-button]')) {
+            const ownerName = ownerLink.textContent.trim();
+            const button = document.createElement('button');
+            button.textContent = '@';
+            button.style.marginLeft = '5px';
+            button.dataset.ownerButton = true;
+            button.onclick = function() {
+                const input = document.getElementById('mission_reply_content');
+                input.value += '@' + ownerName + ' ';
+            };
+
+            // Stile für den Darkmode hinzufügen, wenn aktiv
+            if (isDarkMode()) {
+                button.style.backgroundColor = '#505050';
+                button.style.color = '#ffffff';
+            } else {
+                // Falls Darkmode nicht aktiv, reguläre Stile anwenden
+                button.style.backgroundColor = '#fafafa';
+                button.style.color = '#000000';
+            }
+
+            ownerLink.parentElement.appendChild(button);
+        }
+    }
+
+    // Funktion zum Hinzufügen des Buttons für die Fahrzeuge in der Tabelle mit der ID "mission_vehicle_driving"
+    function addButtonToDrivingVehicles() {
+        const rows = document.querySelectorAll('tr[id^="vehicle_row_"]');
+
+        rows.forEach(row => {
+            const ownerLink = row.querySelector('td:nth-child(7) a[href^="/profile/"]');
+            addButtonToOwner(ownerLink);
+        });
+    }
+
+    // Funktion zum Hinzufügen des Buttons für die Fahrzeuge in der Haupttabelle
+    function addButtonToMainTable() {
+        const rows = document.querySelectorAll('tr[id^="vehicle_row_"]');
+
+        rows.forEach(row => {
+            const ownerLink = row.querySelector('td:nth-child(5) a[href^="/profile/"]');
+            addButtonToOwner(ownerLink);
+        });
+    }
+
+    addButtonToMainTable();
+    addButtonToDrivingVehicles();
+})();
